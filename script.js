c3cf55fc995458aeed73a61cb073e7e6d0581ec0
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-04-30 16:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    // Retrieve a reference to the table
+    var table = document.getElementById('sampleTable');
+
+    // Create a new row
+    var newRow = table.insertRow();
+
+    // Create cell elements and set their content
+    var cell1 = newRow.insertCell(0);
+    cell1.innerHTML = 'New Cell 1';
+
+    var cell2 = newRow.insertCell(1);
+    cell2.innerHTML = 'New Cell 2';
 }
