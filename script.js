9a2e1989d83c795751e7e9ccbd5eaf7fc27f4956
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-06 02:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function insert_Row() {
 
     // Create cell elements and set their content
     var cell1 = newRow.insertCell(0);
-    cell1.innerHTML = 'New Cell 1';
+    cell1.innerHTML = 'New Cell1';
 
     var cell2 = newRow.insertCell(1);
-    cell2.innerHTML = 'New Cell 2';
+    cell2.innerHTML = 'New Cell2';
 }
